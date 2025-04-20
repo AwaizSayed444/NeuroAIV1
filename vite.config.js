eb81154ca9846c7b3cd75d9fcd4a99d8a3eb3c00
Awaiz-Sayed <sawaiz0207@gmail.com>
2025-04-20 20:20:42 +0530

    Fixed entry point and updated index.html for Vite React app

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
   base: "/NeuroAIV1",
-=======
-  base: '/NeuroAIV1',
->>>>>>> 661d5abb5c6017195871756a982851ed187e9592
   plugins: [react()],
 });
